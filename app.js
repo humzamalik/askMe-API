5f946df7c4c1715ef15638218ec01989443637b6
humzamalik <hamzaarshad611@gmail.com>
2020-12-28 06:44:28 +0500

    adding error handling

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ const answerRoutes = require("./api/routes/answers")
 app.use("/questions", questionRoutes)
 app.use("/answers", answerRoutes)
 
+app.use((req, res, next) => {
+    const err = new Error("Not Found")
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        code: err.status
+    })
+})
+
 module.exports = app
