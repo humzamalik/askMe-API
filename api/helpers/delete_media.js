9d969fde1df2b3fb1080fa1af1536c0f5fc16298
humzamalik <humzaarshad611@gmail.com>
2021-02-11 06:43:41 +0500

    fix: media deletion

--- api/helpers/delete_media.js
+++ api/helpers/delete_media.js
@@ -1,8 +1,7 @@
 import fs from "fs"
 
 const delFile = (path) => {
-    console.log(path)
-    fs.unlink(`../../${path}`, (err) => {
+    fs.unlink(path, (err) => {
         if (err) {
             console.error(err)
             return
